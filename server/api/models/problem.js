2f8cb46f46ebbfc8a86152965ad6acf01a2947b8
Erfan <eib.erfan@gmail.com>
2021-03-15 14:18:08 +0600

    added: problem scrapper

--- server/api/models/problem.js
+++ server/api/models/problem.js
@@ -4,6 +4,7 @@ const problemSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   judge: { type: String, required: true },
   problemID: { type: String, required: true },
+  title: { type: String, required: true },
   solved: { type: Number, default: 0 },
 });
 
